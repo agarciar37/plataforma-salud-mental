9ddca315d4fd12c8f8a7cf6db4bf635688ee4bac
agarciar37 <agarciar37@alumnos.nebrija.es>
2026-05-10 22:54:44 +0200

    Update CORS for production frontensd

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://plataforma-salud-mental.vercel.app"
+        "https://plataforma-salud-mental.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
